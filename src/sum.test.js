cd9045bcaa63ecd7e42ab9ea664a4dfe6f54f6fd
Md. Ibrahim Hafeez <mdibrahimhaf6996@gmail.com>
2023-05-02 21:30:28 +0530

    Added Tests

---
+++ src/sum.test.js
@@ -0,0 +1,12 @@
+import sum from './sum'
+import { describe, it, expect } from 'vitest'
+
+describe("sum functin test",()=>{
+    it("returns 0 with no numbers", ()=>{
+        expect(sum()).toBe(0)
+    })
+
+    it("returns same number with 1 numbers", ()=>{
+        expect(sum(1)).toBe(1)
+})
+})
