6cbf044822a55e1984e3708cc963cc3ef6a276ea
Md. Ibrahim Hafeez <mdibrahimhaf6996@gmail.com>
2023-05-02 21:59:41 +0530

    Added workflows testing yml file

--- src/sum.test.js
+++ src/sum.test.js
@@ -8,5 +8,9 @@ describe("sum functin test",()=>{
 
     it("returns same number with 1 numbers", ()=>{
         expect(sum(1)).toBe(1)
-})
+    })
+
+    it("returns sum number with 2 numbers", ()=>{
+        expect(sum(1, 2)).toBe(3)
+    })
 })
